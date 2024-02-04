d46a3b120df02c028ead195722b06ffae8588e64
Ayush Paudel <ayushpaudel20@gmail.com>
2024-02-04 16:51:16 +0545

    build and add respond to mentions feature

---
+++ go.mod
@@ -0,0 +1,10 @@
+module slack-bot-test
+
+go 1.20
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/slack-go/slack v0.12.3
+)
+
+require github.com/gorilla/websocket v1.4.2 // indirect
---
+++ main.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/joho/godotenv"
+	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/slackevents"
+	"github.com/slack-go/slack/socketmode"
+)
+
+func main() {
+	godotenv.Load(".env")
+
+	token := os.Getenv("SLACK_AUTH_TOKEN")
+	appToken := os.Getenv("SLACK_APP_TOKEN")
+	slackClient := slack.New(token, slack.OptionDebug(true), slack.OptionAppLevelToken(appToken))
+
+	socketClient := socketmode.New(
+		slackClient,
+		socketmode.OptionDebug(false),
+		// Option to set a custom logger
+		socketmode.OptionLog(log.New(os.Stdout, "socketmode: ", log.Lshortfile|log.LstdFlags)),
+	)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	go processNewEvents(ctx, slackClient, socketClient)
+	socketClient.Run()
+
+}
+
+func sendSlackMessage(slackClient slack.Client) {
+	channelId := os.Getenv("SLACK_CHANNEL_ID")
+
+	slackAttachment := slack.Attachment{
+		Pretext: "Super Bot Message",
+		Color:   "#36a64f",
+		Fields: []slack.AttachmentField{
+			{
+				Title: "Date",
+				Value: time.Now().String(),
+				Short: true,
+			},
+			{
+				Title: "Message",
+				Value: "Hello there",
+				Short: true,
+			},
+			{
+				Title: "Test",
+				Value: "Hello here",
+				Short: true,
+			}, {
+				Title: "Test",
+				Value: "Hello here",
+				Short: true,
+			}, {
+				Title: "Test",
+				Value: "Hello here",
+				Short: true,
+			},
+		},
+	}
+
+	res, res2, err := slackClient.PostMessage(
+		channelId, slack.MsgOptionAttachments(slackAttachment),
+	)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print(res)
+	fmt.Print(res2)
+}
+
+// processNewEvents will be reading from Events channel
+func processNewEvents(ctx context.Context, client *slack.Client, socketClient *socketmode.Client) {
+	for {
+		select {
+		case <-ctx.Done():
+			log.Println("Shutting down events listener.")
+			return
+		case event := <-socketClient.Events:
+			switch event.Type {
+			case socketmode.EventTypeEventsAPI:
+				eventsAPIEvent, ok := event.Data.(slackevents.EventsAPIEvent)
+				if !ok {
+					log.Printf("Could not type cast the event to the EventsAPIEvent: %v\n", event)
+					continue
+				}
+				socketClient.Ack(*event.Request)
+				err := handleEventMessage(eventsAPIEvent, client)
+				if err != nil {
+					log.Println(err)
+				}
+			}
+		}
+	}
+}
+
+func handleEventMessage(event slackevents.EventsAPIEvent, slackClient *slack.Client) error {
+	switch event.Type {
+	case slackevents.CallbackEvent:
+		innerEvent := event.InnerEvent
+		switch ev := innerEvent.Data.(type) {
+		case *slackevents.AppMentionEvent:
+			err := handleAppMentionEvent(ev, slackClient)
+			if err != nil {
+				return err
+			}
+		}
+	default:
+		return errors.New("unsupported event type")
+	}
+	return nil
+}
+
+func handleAppMentionEvent(event *slackevents.AppMentionEvent, slackClient *slack.Client) error {
+	// Grab the user name based on the ID of the one who mentioned the bot
+	user, err := slackClient.GetUserInfo(event.User)
+	if err != nil {
+		return err
+	}
+	text := strings.ToLower(event.Text)
+	attachment := slack.Attachment{
+		Fields: []slack.AttachmentField{
+			{
+				Title: "Date",
+				Value: time.Now().Local().String(),
+			},
+			{
+				Title: "Initializer",
+				Value: user.Name,
+				Short: true,
+			},
+		},
+	}
+	if strings.Contains(text, "hello") {
+		attachment.Text = fmt.Sprintf("Hello, %s", user.RealName)
+		attachment.Pretext = "Greetings"
+		attachment.Color = "#4af030"
+	} else {
+		// Send a message to the user
+		attachment.Text = fmt.Sprintf("How can I help you %s?", user.RealName)
+		attachment.Pretext = "How can I be of service?"
+		attachment.Color = "#3d3d3d"
+	}
+	channelId := os.Getenv("SLACK_CHANNEL_ID")
+	_, _, err = slackClient.PostMessage(channelId, slack.MsgOptionAttachments(attachment))
+	if err != nil {
+		return err
+	}
+	return nil
+}
